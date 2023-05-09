206a56638fe7bb05d3bbf0738d71d053e2a7d759
Pian <alifalpian157@gmail.com>
2023-05-09 14:48:17 +0700

    code

--- Episode 00-Hello World/main.py
+++ Episode 00-Hello World/main.py
@@ -1,1 +1,14 @@
-print("Hello World!")
+
+print("Hello world!")
+
+# pagar = comment di python
+
+"""
+multi-line 
+comment
+"""
+
+import time
+start_time = time.time()
+
+print(time.time() - start_time, "Detik")
