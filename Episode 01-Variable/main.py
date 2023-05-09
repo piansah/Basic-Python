260eb76d81e60ce302530ce47aec96195d9ec21b
Pian <alifalpian157@gmail.com>
2023-05-09 15:05:17 +0700

    Code

--- Episode 01-Variable/main.py
+++ Episode 01-Variable/main.py
@@ -1,6 +1,7 @@
 # Variable
 
 """
+Variabel adalah tempat untuk menyimpan data
 x = 2
 y = x+4 (2+4)
 y = 6
@@ -9,3 +10,20 @@ x adalah variable
 2 adalah value
 """
 
+#Assingnment Value
+a = 10
+x = 5
+panjang = 1000
+
+print("Nilai a =", a)
+print("Nilai a =", x)
+print("Nilai a =", panjang)
+
+#pemanggilan kedua
+print("Nilai a =", a)
+a = 7
+print("Nilai a =", a)
+
+#Assignment Indirect
+b = a
+print("Nilai b =", b)
