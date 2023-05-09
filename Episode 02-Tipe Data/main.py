4e150636e5cd2bee0d64db32596c175432587d7d
Pian <alifalpian157@gmail.com>
2023-05-09 15:20:08 +0700

    Code

---
+++ Episode 02-Tipe Data/main.py
@@ -0,0 +1,27 @@
+# Tipe Data
+
+# Tipe data: Angka satuan (Integer)
+data_int = 1
+print ("data = ", data_int)
+print("bertipe =", type(data_int))
+
+# Tipe data: Angka dengan koma (Float)
+data_float = 1.1
+print ("data = ", data_float)
+print("bertipe =", type(data_float))
+
+# Tipe data: Kumpulan katakter (String)
+data_string = "ucup"
+print ("data = ", data_string)
+print("bertipe =", type(data_string))
+
+# Tipe data: binner true or false (Boolean)
+data_bool = True
+print ("data = ", data_bool)
+print("bertipe =", type(data_bool))
+
+## Tipe data khusus
+# bilangan kompleks
+data_complex = complex(5,6)
+print ("data = ", data_complex)
+print("bertipe =", type(data_complex))
